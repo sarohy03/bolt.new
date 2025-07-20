b8215c95b133360d74c71cb5d373b6a6ee76ccc7
Abdul Hannan Sarohy <123007385+sarohy03@users.noreply.github.com>
2025-07-20 12:38:07 +0500

    Fix renderToReadableStream import and add sass dependency

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,7 +1,8 @@
 import type { AppLoadContext, EntryContext } from '@remix-run/cloudflare';
 import { RemixServer } from '@remix-run/react';
 import { isbot } from 'isbot';
-import { renderToReadableStream } from 'react-dom/server';
+import pkg from 'react-dom/server';
+const { renderToReadableStream } = pkg;
 import { renderHeadToString } from 'remix-island';
 import { Head } from './root';
 import { themeStore } from '~/lib/stores/theme';
